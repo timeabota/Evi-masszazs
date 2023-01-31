745f320c9e6ac24a701b7b0ffabbbe8b19b92fa2
timeabota <timea.bota@gmail.com>
2023-01-31 07:48:54 +0100

    css animation

--- script.js
+++ script.js
@@ -3,23 +3,5 @@ const navList = Array.from(document.querySelectorAll('.nav-list'))[0];
 
 toggleButton.addEventListener('click', () => toggleNav());
 
-
-// function toggleNav() {
-//     const navClass = navList.classList;
-    
-//     if (navClass.classList.indexOf("kiscica") > -1) {
-//         navClass.remove("kiscica");
-//     } else {
-//         navClass.add("kiscica");
-//     }
-// }
-
-// function toggleNav() {
-//     const navClass = navList.classList;
-    
-//     navClass.classList.indexOf("kiscica") ? navClass.remove("kiscica") : navClass.add("kiscica");
-
-// }
-
 const toggleNav = () => navList.classList.toggle('active');
 
